3bf8edef0b3a081fee28d1c51571cdcbc59287af
Simone Demuro <simone.demuro@techedge.com>
2018-10-15 19:31:15 +0200

    Project Added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# adminlte-lite
-Compact version of AdminLTE dashboard template
+# adminlte-lite .Net MVC
+.Net Framework MVC Application preconfigured with the
+Compact version of AdminLTE dashboard template.
---
+++ WebAppMVCTemplate/Controllers/TemplateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMVCTemplate.Controllers
+{
+    public class TemplateController : Controller
+    {
+        // GET: Template
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: Template
+        public ActionResult FullDashboard()
+        {
+            return View();
+        }
+    }
+}
